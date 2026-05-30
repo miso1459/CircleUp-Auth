04659d9406b323022b26a85c284fd1726df537be
kj.kang <37363887+miso1459@users.noreply.github.com>
2026-05-30 19:49:35 +0900

    feat: Add Check menu item to Generator dropdown
    - Add /gen/Check link in NavLeft Generator dropdown (admin only)
    - Positioned after Translation, before closing </ul>
    - Consistent pattern: NavigationMenu.Link + snippet child + resolve()

--- src/routes/_components/NavLeft.svelte
+++ src/routes/_components/NavLeft.svelte
@@ -205,6 +205,15 @@
 								{/snippet}
 							</NavigationMenu.Link>
 						</li>
+						<li>
+							<NavigationMenu.Link>
+								{#snippet child({ props })}
+									<a href={resolve('/gen/Check' as Pathname)} data-sveltekit-reload {...props}>
+										Check
+									</a>
+								{/snippet}
+							</NavigationMenu.Link>
+						</li>
 					</ul>
 				</NavigationMenu.Content>
 			</NavigationMenu.Item>
