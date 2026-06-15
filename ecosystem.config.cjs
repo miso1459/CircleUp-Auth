0817677488157a9c24eca87a513943ae72dafccb
kj.kang <37363887+miso1459@users.noreply.github.com>
2026-06-15 21:16:00 +0900

    .

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
 
     // 👇 클러스터 모드 활성화 설정
     exec_mode: 'cluster',     // 실행 모드를 클러스터로 지정
-    instances: 'max',         // 'max'로 설정 시 이용 가능한 CPU 코어 수만큼 프로세스 자동 생성 (예: 4코어면 4개)
+    instances: '2',         // 'max'로 설정 시 이용 가능한 CPU 코어 수만큼 프로세스 자동 생성 (예: 4코어면 4개)
 
     watch: false,
     autorestart: true, // 🟢 오류 발생 시 자동으로 1초 만에 재시작해 주는 옵션
